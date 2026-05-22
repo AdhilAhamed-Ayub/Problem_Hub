2de56f32575adf7eee6e43c8a23342a16ac41dda
AdhilAhamed_Ayub <149603498+Ahamedayub@users.noreply.github.com>
2026-05-22 23:41:54 +0530

    Day 2 - Reverse the Number

--- JAVA/Basics/practise/src/problems_number/Reverse_num.java
+++ JAVA/Basics/practise/src/problems_number/Reverse_num.java
@@ -2,22 +2,25 @@ package problems_number;
 
 import java.util.*;
 import java.lang.*;
+
 public class Reverse_num {
+
+	public static int rev(int num) {
+		int rev_num = 0;
+		while (num != 0) {
+			int digit = num % 10; // Extract the last digit (e.g., 4)
+			rev_num = rev_num * 10 + digit; // Append it to the reversed number
+			num /= 10; // Remove the last digit from original (e.g., 123)
+		}
+		return rev_num;
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("eneter the array size:");
+		System.out.println("eneter the number:");
 		int n = sc.nextInt();
-		int [] a = new int[n];
-		for(int i=0;i<n;i++) {
-			a[i]=sc.nextInt();
-		}
-		System.out.println(Arrays.toString(a));
-		
-		
-		
-		
-		
+		System.out.println("reverse the given number" + rev(n));
+
 	}
 
 }
